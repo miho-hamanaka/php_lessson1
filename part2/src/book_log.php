1fa20eef5b4c4ca2b6425c07954e5e58cfc2c032
miho-hamanaka <miho-hamanaka@se-ec.co.jp>
2021-01-03 04:34:04 +0900

    メニューを繰り返し処理で表示

--- part2/src/book_log.php
+++ part2/src/book_log.php
@@ -6,46 +6,49 @@ $status = '';
 $evaluation = '';
 $comment = '';
 
-echo '1. 読書ログを登録' . PHP_EOL;
-echo '2. 読書ログを表示' . PHP_EOL;
-echo '9. アプリケーションを終了' . PHP_EOL;
-echo '番号を選択してください(1,2,9):';
+while (true) {
 
-$num = trim(fgets(STDIN));
+    echo '1. 読書ログを登録' . PHP_EOL;
+    echo '2. 読書ログを表示' . PHP_EOL;
+    echo '9. アプリケーションを終了' . PHP_EOL;
+    echo '番号を選択してください(1,2,9):';
 
-if ($num === '1') {
-    //読書ログを登録
-    echo '読書ログを登録してください' . PHP_EOL;
-    echo '書籍名:';
-    $title = trim(fgets(STDIN));
+    $num = trim(fgets(STDIN));
 
-    echo '著者名:';
-    $author = trim(fgets(STDIN));
+    if ($num === '1') {
+        //読書ログを登録
+        echo '読書ログを登録してください' . PHP_EOL;
+        echo '書籍名:';
+        $title = trim(fgets(STDIN));
 
-    echo '読書状況（未読,読んでる,読了）:';
-    $status = trim(fgets(STDIN));
+        echo '著者名:';
+        $author = trim(fgets(STDIN));
 
-    echo '評価（5点満点の整数）:';
-    $evaluation = trim(fgets(STDIN));
+        echo '読書状況（未読,読んでる,読了）:';
+        $status = trim(fgets(STDIN));
 
-    echo '感想:';
-    $comment = trim(fgets(STDIN));
+        echo '評価（5点満点の整数）:';
+        $evaluation = trim(fgets(STDIN));
 
-    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
-    
-} elseif ($num === '2'){
-    //読書ログを表示
-    echo '読書ログを表示します' . PHP_EOL;
-    echo '書籍名:' . $title . PHP_EOL;
-    echo '著者名:' . $author . PHP_EOL;
-    echo '読書状況:' . $status . PHP_EOL;
-    echo '評価:' . $evaluation . PHP_EOL;
-    echo '感想:' . $comment . PHP_EOL;
+        echo '感想:';
+        $comment = trim(fgets(STDIN));
 
-} elseif ($num === '9') {
-    //アプリケーションを終了する
+        echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+        
+    } elseif ($num === '2'){
+        //読書ログを表示
+        echo '読書ログを表示します' . PHP_EOL;
+        echo '書籍名:' . $title . PHP_EOL;
+        echo '著者名:' . $author . PHP_EOL;
+        echo '読書状況:' . $status . PHP_EOL;
+        echo '評価:' . $evaluation . PHP_EOL;
+        echo '感想:' . $comment . PHP_EOL;
+
+    } elseif ($num === '9') {
+        //アプリケーションを終了する
+        break;
+    }
 }
-
 // echo '書籍名: 銀河鉄道の夜' . PHP_EOL;
 // echo '著者名: 宮沢賢治' . PHP_EOL;
 // echo '読書状況: 読了' . PHP_EOL;
